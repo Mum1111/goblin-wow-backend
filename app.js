aa0058a5884e6f884e005811507fcb9e2c9afdb2
shihongjin <543528285@qq.com>
2022-05-26 17:42:39 +0800

    feat: 配置接口请求频率

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const cors = require('@koa/cors')
 const Koa = require('koa')
 const koaBodyparser = require('koa-bodyparser')
 const InitManager = require('./core/init')
+const ratelimit = require('koa-ratelimit');
+
 
 require('module-alias/register')
 
@@ -13,6 +15,29 @@ const app = new Koa()
 app.use(cors())
 app.use(catchError)
 app.use(koaBodyparser())
+
+const db = new Map()
+
+app.use(ratelimit({
+    driver: 'memory',
+    db: db,
+    duration: 60000,
+    errorMessage: 'Sometimes You Just Have to Slow Down.',
+    id: (ctx) => ctx.ip,
+    headers: {
+      remaining: 'Rate-Limit-Remaining',
+      reset: 'Rate-Limit-Reset',
+      total: 'Rate-Limit-Total'
+    },
+    max: 100,
+    disableHeader: false,
+    whitelist: (ctx) => {
+      // some logic that returns a boolean
+    },
+    blacklist: (ctx) => {
+      // some logic that returns a boolean
+    }
+  }));
 
 InitManager.initCore(app)
 
